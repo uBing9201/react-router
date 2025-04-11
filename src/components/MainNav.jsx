88bcf5dca595d0dea15ba44b0103dc91c9ea8152
ubing <ubing0101@kakao.com>
2025-04-11 14:39:28 +0900

    #4. NavLink 사용하기

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styles from './MainNav.module.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const MainNav = () => {
+  const activateLink = ({ isActive }) =>
+    `${styles.navLink} ${isActive ? styles.active : ''}`;
+  /*
+    NavLink는 클래스 작성란에 콜백 함수를 선언할 수 있음.
+    콜백 함수의 매개값으로 객체가 전달, 그 객체에 NavLink의 기능을 제어할 수 있는 프로퍼티 존재
+    isActive: 현재 페이지 인지를 확인할 수 있게 하는 값
+  */
+
   return (
     <nav className={styles.nav}>
-      <Link className={styles.navLink} to='/'>
+      <NavLink className={activateLink} to='/'>
         Home
-      </Link>
-      <Link className={styles.navLink} to='/blog'>
+      </NavLink>
+      <NavLink className={activateLink} to='/blog'>
         Blog
-      </Link>
-      <Link className={styles.navLink} to='/about'>
+      </NavLink>
+      <NavLink className={activateLink} to='/about'>
         About
-      </Link>
+      </NavLink>
     </nav>
   );
 };
